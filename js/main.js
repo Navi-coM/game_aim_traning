3bb11f99a86dca933d2504870e6784f2693a680d
Navi-coM <navicom17@gmail.com>
2026-05-20 22:05:30 +0300

    add new script for Circle

--- js/main.js
+++ js/main.js
@@ -32,19 +32,20 @@ listSizes.addEventListener('click', (e) => {
         board.style.width = e.target.dataset.width + 'px';
         board.style.height = e.target.dataset.height + 'px';
     };
-    
+
 });
 
 function startGame() {
-    setInterval(decreaseTime, 1000);  // Написать код который запустит обратный отсчет до 0 с интервалом 1 сек
+    setInterval(decreaseTime, 1000);
+    createRandomCircle();
 }
 
 function decreaseTime() {
-    if(time === 0) {
-        // finishGame();
+    if (time === 0) {
+        finishGame();
     } else {
         let currentTime = --time;
-        if(currentTime < 10) {
+        if (currentTime < 10) {
             currentTime = `0${currentTime}`;
         }
         setTime(currentTime);
@@ -55,4 +56,34 @@ function setTime(value) {
     timeCounter.innerHTML = `00:${value}`;
 }
 
+function finishGame() {
+    timeCounter.parentNode.classList.add('hide');
+    board.innerHTML = `<h1>Счёт: <span class="primary">${score}</span></h1>`
+}
 
+function createRandomCircle() {
+    const circle = document.createElement('div');
+    const size = getRandomnumber(15, 40);
+    const { width, height } = board.getBoundingClientRect();
+    const x = getRandomnumber(0, width - size);
+    const y = getRandomnumber(0, height - size);
+    // 
+
+    circle.classList.add('circle');
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.top = `${y}px`;
+    circle.style.left = `${x}px`;
+    // 
+
+    board.append(circle);
+}
+
+function getRandomnumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+//  ДЗ -----------------------------------
+
+// 1. Написать функцию которая будет брать массив цветов colors и генераировать рандомный 
+// цвет из тех что в массиве и добавить этот цвет в createRandomCircle
